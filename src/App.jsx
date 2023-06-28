e2b6aa2da4062cc157e71fa54785262ba15471d0
Martu Riesco <marturiesco.costa@hotmail.com>
2023-06-28 12:00:19 -0300

    add Forms and Contacto components and styles. Install npm packages.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter,Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Index from './components/Index/Index'
 import Obras from './components/Obras/Obras'
+import Contacto from './components/Contacto/Contacto'
 function App() {
   
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Index/>}></Route>
           <Route path="/obras" element={<Obras/>}></Route>
+          <Route path='/contacto' element={<Contacto/>}></Route>
         </Routes>
       </main>
     </div>
--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import './style.css'
+import Forms from '../Forms/Forms'
+import {FiMapPin} from 'react-icons/fi'
+import {BsWhatsapp} from 'react-icons/bs'
 
 function Contacto() {
   return (
-    <div>Contacto</div>
+    <div>
+      <div className='mapa'>
+        <img className='imagen-mapa'src="https://i.imgur.com/uvSPtzJ.png" alt="ubicacion_estudio" />
+      </div>
+      <section className='informacion'>
+        <div className='informacion-contacto'>
+          <h2 className='titulo-contacto'>
+            Contáctenos <br /> para más <br /> info.
+          </h2>
+          <hr />
+          <p className='descripcion-contacto'>
+          Lorem ipsum dolor sit amet,<br /> consectetur adipiscing elit,
+          sed do eiusmod tempor <br />incididunt ut labore et dolore magna aliqua.
+          </p>
+          <h3 className='ubicacion-contacto'>
+            Buenos Aires, Argentina
+          </h3>
+          <div className='iconos-contacto'>
+            <div className='iconos-informacion'>
+            <FiMapPin className='icon'/>
+            <h4 className='direccion-contacto'>
+            Ibera 2599 <br /> Piso 8
+            </h4>
+            </div>
+            <div className='iconos-informacion'>
+            <BsWhatsapp className='icon'/>
+            <h4 className='direccion-contacto'>
+            +54911 4034-7047
+            </h4>
+
+            </div>
+            <div className='iconos-informacion'>
+            <BsWhatsapp className='icon'/>
+            <h4 className='direccion-contacto'>
+            +54911 4542-2848
+            </h4>
+
+            </div>
+          </div>
+
+
+
+        </div>
+        <div className='forms'>
+          <Forms/>
+        </div>
+      </section>
+
+    </div>
   )
 }
 
