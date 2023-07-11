3a8a3a2a58c9b1f7e2766b21e6a0582c01ae05f9
martin-centurion <martinalejandrocenturion@gmail.com>
2023-07-10 23:50:44 -0300

    edit style component WhatsApp and ObrasListDetail

--- src/components/ObrasListDetail/ObrasListDetail.jsx
+++ src/components/ObrasListDetail/ObrasListDetail.jsx
@@ -27,56 +27,92 @@ function ObrasListDetail() {
   
   // eslint-disable-next-line react/jsx-key
   <div className='detail__content'>
-          <div className='detail__content-img' key={user.id}>
-              <div className='detail__content-title'>
-                  <h1>{user.title}</h1>
-                  <hr className="hr" /><div>
-                <p className="detail-work">
-                  {user.work}
-                </p>
-              </div>
-              
-              </div>
-              <div className="swiper-detail"> <Swiper
-     slidesPerView={'3'}
-     centeredSlides={true}
-     spaceBetween={500}
-     loop={true}
-     pagination={{
-      clickable: true,
-    }}
-     navigation={true}
-        
-       
-        modules={[Navigation,Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide> <img src={user.img1} alt={user.title} className="imagen_detail" /></SwiperSlide>
-        <SwiperSlide> <img src={user.img2} alt={user.title} className="imagen_detail" /></SwiperSlide>
-        <SwiperSlide> <img src={user.img3} alt={user.title} className="imagen_detail" /></SwiperSlide>
-        <SwiperSlide> <img src={user.img4} alt={user.title} className="imagen_detail" /></SwiperSlide>
-        <SwiperSlide> <img src={user.img5} alt={user.title} className="imagen_detail" /></SwiperSlide>
-        <SwiperSlide> <img src={user.img6} alt={user.title} className="imagen_detail" /></SwiperSlide>
-        <SwiperSlide> <img src={user.img7} alt={user.title} className="imagen_detail" /></SwiperSlide>
-        <SwiperSlide> <img src={user.img8} alt={user.title} className="imagen_detail" /></SwiperSlide>
-        <SwiperSlide> <img src={user.img9} alt={user.title} className="imagen_detail" /></SwiperSlide>
-        <SwiperSlide> <img src={user.img10} alt={user.title} className="imagen_detail" /></SwiperSlide>
-      </Swiper>
-                 
-              </div>
-              <div className='detail__content-subtitle'>
-                  <h4>Descripción</h4>
-              <div className='detail__content-parrafo'>
-                  <p>{user.description}</p>  
-                  <hr className="hr2" />
-              </div>       
-              </div>
-              <div className="detail_content-ubication">
-                <p>
-                  {user.ubication}
-                </p>
+                <div className='detail__content-img' key={user.id}>
+                  <div className='detail__content-title'>
+                      <h1>{user.title}</h1>
+                      <hr className="hr"/> <div>
+                      <p className="detail-work">{user.work}</p>
+                  </div>
+                </div>
 
-              </div>
+                <div className="swiper-detail"> 
+              
+                    <Swiper
+                              slidesPerView={'2'}
+                              centeredSlides={true}
+                              spaceBetween={5}
+                              loop={true}
+                              pagination={{
+                                clickable: true,
+                              }}
+                              navigation={true}
+                              modules={[Navigation, Pagination]}
+                              className="mySwiper"
+                    >
+
+                        <SwiperSlide>
+                          <div className='content_imagen'>
+                              <img src={user.img1} alt={user.title} className="imagen_detail" />
+                          </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                          <div className='content_imagen'>
+                              <img src={user.img2} alt={user.title} className="imagen_detail" />
+                          </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                          <div className='content_imagen'>
+                              <img src={user.img3} alt={user.title} className="imagen_detail" />
+                          </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                          <div className='content_imagen'>
+                              <img src={user.img4} alt={user.title} className="imagen_detail" />
+                          </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                          <div className='content_imagen'>
+                              <img src={user.img5} alt={user.title} className="imagen_detail" />
+                          </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                          <div className='content_imagen'>
+                              <img src={user.img6} alt={user.title} className="imagen_detail" />
+                          </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                          <div className='content_imagen'>
+                              <img src={user.img7} alt={user.title} className="imagen_detail" />
+                          </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                          <div className='content_imagen'>
+                              <img src={user.img8} alt={user.title} className="imagen_detail" />
+                          </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                          <div className='content_imagen'>
+                              <img src={user.img9} alt={user.title} className="imagen_detail" />
+                          </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                          <div className='content_imagen'>
+                              <img src={user.img10} alt={user.title} className="imagen_detail" />
+                          </div>
+                        </SwiperSlide>
+                    </Swiper>
+                 
+                </div>
+          </div>
+          <div className='detail__content-subtitle'>
+                    <h4>Descripción</h4>
+                <div className='detail__content-parrafo'>
+                    <p>{user.description}</p>  
+                    <hr className="hr2" />
+                </div>       
+                </div>
+                <div className="detail_content-ubication">
+                  <p>{user.ubication}</p>
           </div>
   </div>
     ))}
